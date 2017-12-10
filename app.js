fcc96fd2e77d60119adf62ca5b0c6816bedb9f22
ujfator <33957024+ujfator@users.noreply.github.com>
2017-12-10 09:39:35 +0100

    copied Angular todolist from https://www.w3schools.com/angular/tryit.asp?filename=try_ng_todo_app, now I will decipher all that shit

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp', []); 
+app.controller('todoList', function($scope) {
+    $scope.todoList = [{todoText:'Wake the FUCK UP', done:false}];
+
+    $scope.todoAdd = function() {
+        $scope.todoList.push({todoText:$scope.todoInput, done:false});
+        $scope.todoInput = "";
+    };
+
+    $scope.remove = function() {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function(x) {
+            if (!x.done) $scope.todoList.push(x);
+        });
+    };
+});
